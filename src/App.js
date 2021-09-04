d3310c7ee885bf5e56c9926a8d4b0b48841b9b2b
Sam <85004512+aar2dee2@users.noreply.github.com>
2021-09-04 16:14:08 +0000

    Wrote functions to check for wallets and connect a wallet

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { ethers } from "ethers";
 import './App.css';
 
 export default function App() {
+  // Creating a state variable to store the user's public wallet address
+  const [currAccount, setCurrentAccount] = React.useState("")
+
   const checkIfWalletIsConnected = () => {
     const { ethereum } = window;
     if (!ethereum) {
@@ -11,15 +14,46 @@ export default function App() {
     } else {
       console.log("We have the ethereum object", ethereum)
     }
+
+  // Check if we're authorized to access the user's wallet
+  ethereum.request({ method: 'eth_accounts' })
+  .then(accounts => {
+    //There could be multiple accounts, check for one.
+    if(accounts.length !== 0) {
+      //Grab the first account we have access to.
+      const account = accounts[0];
+      console.log("Found an authorized account: ", account)
+
+      //Store the user's public wallet address for later!
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found")
+    }
+  })
+
+}
+
+const connectWallet = () => {
+  const { ethereum } = window;
+  if (!ethereum) {
+    alert("Get metamask")
   }
+   ethereum.request({ method: "eth_requestAccounts" })
+   .then(accounts => {
+     console.log("Connected", accounts[0])
+     setCurrentAccount(accounts[0])
+   })
+   .catch(err => console.log(err));
+}
 
-  React.useEffect(() => {
-    checkIfWalletIsConnected()
-  }, [])
+React.useEffect(() => {
+  checkIfWalletIsConnected()
+}, [])
 
-  const wave = () => {
+const wave = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
     
-  }
+}
   
   return (
     <div className="mainContainer">
@@ -36,6 +70,15 @@ export default function App() {
         <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
+
+        {currAccount ? null : (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )
+
+        }
+
       </div>
     </div>
   );
